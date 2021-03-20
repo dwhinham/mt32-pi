dwhinham/mt32-pi

commit 14f9506c6d5b257953ed3695068e17785bd958cc

Fix logging via serial port

src/kernel.cpp
@@ -57,15 +57,17 @@ bool CKernel::Initialize(void)
 		return false;
 #endif
 
-	CDevice* pLogTarget = mDeviceNameService.GetDevice(mOptions.GetLogDevice(), false);
+	const char* pLogDeviceName = mOptions.GetLogDevice();
+	const bool bSerialMIDIEnabled = strcmp(pLogDeviceName, "ttyS1") != 0;
+
+	// Use MIDI baud if serial port not in-use for logging
+	if (!m_Serial.Initialize(bSerialMIDIEnabled ? 31250 : 115200))
+		return false;
+
+	CDevice* pLogTarget =  mDeviceNameService.GetDevice(pLogDeviceName, false);
 
 	if (!pLogTarget)
 		pLogTarget = &mNullDevice;
-
-	// Init serial port early if used for logging
-	bool bSerialMIDIEnabled = pLogTarget != &m_Serial;
-	if (!bSerialMIDIEnabled && !m_Serial.Initialize(115200))
-		return false;
 
 	if (!m_Logger.Initialize(pLogTarget))
 		return false;
@@ -90,10 +92,6 @@ bool CKernel::Initialize(void)
 	// Load configuration file
 	if (!m_Config.Initialize("mt32-pi.cfg"))
 		m_Logger.Write(GetKernelName(), LogWarning, "Unable to find or parse config file; using defaults");
-
-	// Init serial port for GPIO MIDI if not being used for logging
-	if (bSerialMIDIEnabled && !m_Serial.Initialize(m_Config.MIDIGPIOBaudRate))
-		return false;
 
 	// Init I2C; don't bother with Initialize() as it only sets the clock to 100/400KHz
 	m_I2CMaster.SetClock(m_Config.SystemI2CBaudRate);

commit df91f53555de6e175c5d06df6818c57bf8b6b87d

Fix broken gpio_baud_rate option
The serial baud rate option was broken since 14f9506.

src/kernel.cpp
@@ -60,8 +60,8 @@ bool CKernel::Initialize(void)
 	const char* pLogDeviceName = mOptions.GetLogDevice();
 	const bool bSerialMIDIEnabled = strcmp(pLogDeviceName, "ttyS1") != 0;
 
-	// Use MIDI baud if serial port not in-use for logging
-	if (!m_Serial.Initialize(bSerialMIDIEnabled ? 31250 : 115200))
+	// Init serial port early if used for logging
+	if (!bSerialMIDIEnabled && !m_Serial.Initialize(115200))
 		return false;
 
 	CDevice* pLogTarget =  mDeviceNameService.GetDevice(pLogDeviceName, false);
@@ -90,6 +90,10 @@ bool CKernel::Initialize(void)
 	// Load configuration file
 	if (!m_Config.Initialize("mt32-pi.cfg"))
 		m_Logger.Write(GetKernelName(), LogWarning, "Unable to find or parse config file; using defaults");
+
+	// Init serial port for MIDI with preferred baud rate if not used for logging
+	if (bSerialMIDIEnabled && !m_Serial.Initialize(m_Config.MIDIGPIOBaudRate))
+		return false;
 
 	// Init I2C; don't bother with Initialize() as it only sets the clock to 100/400KHz
 	m_I2CMaster.SetClock(m_Config.SystemI2CBaudRate);