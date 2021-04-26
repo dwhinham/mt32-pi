6daf6c14be765f1ab55ee6027798398f554e93c1
Dale Whinham <daleyo@gmail.com>
2021-04-26 11:02:55 +0100

    Change default board in Config.mk

--- Config.mk
+++ Config.mk
@@ -6,8 +6,8 @@
 ARM_HOME?=$(HOME)/gcc-arm-10.2-2020.11-x86_64-arm-none-eabi
 AARCH64_HOME?=$(HOME)/gcc-arm-10.2-2020.11-x86_64-aarch64-none-elf
 
-# Valid options: pi0, pi2, pi3, pi4, pi4-64
-BOARD?=pi4
+# Valid options: pi3, pi3-64, pi4, pi4-64
+BOARD?=pi3-64
 HDMI_CONSOLE?=0
 
 # Serial bootloader config
